fa3507ea63712a54378c2337b697e41d9d153166
abcb2 <kouji.wtnb@gmail.com>
2018-11-04 12:31:09 +0900

    test(sample): fix

--- spec/operators/merge-spec.ts
+++ spec/operators/merge-spec.ts
@@ -64,20 +64,33 @@ describe('merge operator', () => {
   //     done();
   //   });
   // });
-  //
-  // it('should merge cold and cold', () => {
-  //   const e1 =  cold('---a-----b-----c----|');
-  //   const e1subs =   '^                   !';
-  //   const e2 =  cold('------x-----y-----z----|');
-  //   const e2subs =   '^                      !';
-  //   const expected = '---a--x--b--y--c--z----|';
-  //
-  //   const result = e1.pipe(merge(e2, rxTestScheduler));
-  //
-  //   expectObservable(result).toBe(expected);
-  //   expectSubscriptions(e1.subscriptions).toBe(e1subs);
-  //   expectSubscriptions(e2.subscriptions).toBe(e2subs);
-  // });
+
+  it('t02', (done) => {
+    const a = of<number>(1, 2, 3, queueScheduler);
+    const b = of<number>(4, 5, 6, 7, 8, queueScheduler);
+    const r = [1, 2, 4, 3, 5, 6, 7, 8];
+    a.pipe(merge(b, queueScheduler)).subscribe(val => {
+      expect(val).to.equal(r.shift());
+    }, x => {
+      done(new Error('should not be called'));
+    }, () => {
+      done();
+    });
+  });
+
+  it('should merge cold and cold', () => {
+    const e1 = cold('---a-----b-----c----|');
+    const e1subs = '^                   !';
+    const e2 = cold('------x-----y-----z----|');
+    const e2subs = '^                      !';
+    const expected = '---a--x--b--y--c--z----|';
+
+    const result = e1.pipe(merge(e2, rxTestScheduler));
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    expectSubscriptions(e2.subscriptions).toBe(e2subs);
+  });
   //
   // it('should merge hot and hot', () => {
   //   const e1 =  hot('---a---^-b-----c----|');
@@ -106,55 +119,75 @@ describe('merge operator', () => {
   //   expectSubscriptions(e1.subscriptions).toBe(e1subs);
   //   expectSubscriptions(e2.subscriptions).toBe(e2subs);
   // });
-  //
-  // it('should merge parallel emissions', () => {
-  //   const e1 =   hot('---a----b----c----|');
-  //   const e1subs =   '^                 !';
-  //   const e2 =   hot('---x----y----z----|');
-  //   const e2subs =   '^                 !';
-  //   const expected = '---(ax)-(by)-(cz)-|';
-  //
-  //   const result = e1.pipe(merge(e2, rxTestScheduler));
-  //
-  //   expectObservable(result).toBe(expected);
-  //   expectSubscriptions(e1.subscriptions).toBe(e1subs);
-  //   expectSubscriptions(e2.subscriptions).toBe(e2subs);
-  // });
-  //
-  // it('should allow unsubscribing explicitly and early', () => {
-  //   const e1 =    hot('--a-----b-----c----|  ');
-  //   const e1subs =    '^         !           ';
-  //   const e2 =    hot('-----d-----e-----f---|');
-  //   const e2subs =    '^         !           ';
-  //   const expected =  '--a--d--b--           ';
-  //   const unsub =     '          !           ';
-  //
-  //   const result = e1.pipe(merge(e2, rxTestScheduler));
-  //
-  //   expectObservable(result, unsub).toBe(expected);
-  //   expectSubscriptions(e1.subscriptions).toBe(e1subs);
-  //   expectSubscriptions(e2.subscriptions).toBe(e2subs);
-  // });
-  //
-  // it('should not break unsubscription chains when result is unsubscribed explicitly', () => {
-  //   const e1 =    hot('--a-----b-----c----|  ');
-  //   const e1subs =    '^         !           ';
-  //   const e2 =    hot('-----d-----e-----f---|');
-  //   const e2subs =    '^         !           ';
-  //   const expected =  '--a--d--b--           ';
-  //   const unsub =     '          !           ';
-  //
-  //   const result = e1.pipe(
-  //     map((x) => x),
-  //     merge(e2, rxTestScheduler),
-  //     map((x) => x)
-  //   );
-  //
-  //   expectObservable(result, unsub).toBe(expected);
-  //   expectSubscriptions(e1.subscriptions).toBe(e1subs);
-  //   expectSubscriptions(e2.subscriptions).toBe(e2subs);
-  // });
-  //
+  it('should merge hot and cold', () => {
+    const e1 =  hot('---a-^---b-----c----|');
+    const e1subs =       '^              !';
+    const e2 = cold(     '--x-----y------z----|');
+    const e2subs =       '^                   !';
+    const expected =     '^-x-b---y-c----z----|';
+    const result = e1.pipe(merge(e2, rxTestScheduler));
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    expectSubscriptions(e2.subscriptions).toBe(e2subs);
+  });
+
+  it('should merge parallel emissions', () => {
+    const e1 =   hot('---a----b----c----|');
+    const e1subs =   '^                 !';
+    const e2 =   hot('---x----y----z----|');
+    const e2subs =   '^                 !';
+    const expected = '---(ax)-(by)-(cz)-|';
+
+    const result = e1.pipe(merge(e2, rxTestScheduler));
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    expectSubscriptions(e2.subscriptions).toBe(e2subs);
+
+    const f1 = hot('--a-----b---c---|');
+    const f2 = hot('--x-----y-------|');
+    const f3 = hot('--z-------------|');
+    const expected2 =
+                   '--(axz)-(by)c---|';
+    const res2 = f1.pipe(merge(f2, f3, rxTestScheduler));
+    expectObservable(res2).toBe(expected2);
+
+  });
+
+  it('should allow unsubscribing explicitly and early', () => {
+    const e1 =    hot('--a-----b-----c----|  ');
+    const e1subs =    '^         !           ';
+    const e2 =    hot('-----d-----e-----f---|');
+    const e2subs =    '^         !           ';
+    const expected =  '--a--d--b--           ';
+    const unsub =     '          !           ';
+
+    const result = e1.pipe(merge(e2, rxTestScheduler));
+
+    expectObservable(result, unsub).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    expectSubscriptions(e2.subscriptions).toBe(e2subs);
+  });
+
+  it('should not break unsubscription chains when result is unsubscribed explicitly', () => {
+    const e1 =    hot('--a-----b-----c----|  ');
+    const e1subs =    '^         !           ';
+    const e2 =    hot('-----d-----e-----f---|');
+    const e2subs =    '^         !           ';
+    const expected =  '--a--d--b--           ';
+    const unsub =     '          !           ';
+
+    const result = e1.pipe(
+      map((x) => x),
+      merge(e2, rxTestScheduler),
+      map((x) => x)
+    );
+
+    expectObservable(result, unsub).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    expectSubscriptions(e2.subscriptions).toBe(e2subs);
+  });
+
   // it('should merge empty and empty', () => {
   //   const e1 = cold('|');
   //   const e1subs = '(^!)';
